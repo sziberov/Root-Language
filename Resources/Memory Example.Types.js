8ba1c20da7c075c9ab1a0d09e715ebb2bf36166d
DIES <sziberov@gmail.com>
2023-06-10 13:13:14 +0300

    Update

--- Resources/Memory Example.Types.js
+++ Resources/Memory Example.Types.js
@@ -1,3 +1,11 @@
+// Rules for a type descriptor parts:
+// - One TDP can have only one collection mask.
+// - TDPs that have collection masks should also have children.
+// - Non-first collection masks are attached as a separate child TDPs.
+// - Masks that are not mutually exclusive can be combined.
+// - Masks can not be ambigously combined (e.g. variadic array type vs array of variadic type).
+// - TDPs should be minimalistic and can not be redundant.
+
 //    inout A! | _? & Global<C>...
 // 0: inout    |               ...
 // 1:       A!
@@ -6,7 +14,7 @@
 // 4:                 Global< >
 // 5:                        C
 
-let type1 = [
+let type0 = [
 	{
 		super: undefined,
 		mask: ['inout', 'union', 'variadic'],
@@ -46,7 +54,7 @@ let type1 = [
 // 3:      B
 // 4:           C
 
-let type2 = [
+let type1 = [
 	{
 		super: undefined,
 		mask: ['intersection'],
@@ -54,7 +62,7 @@ let type2 = [
 	},
 	{
 		super: 0,
-		mask: ['parenthesized', 'union'],
+		mask: ['union'],
 		value: undefined
 	},
 	{
@@ -71,5 +79,131 @@ let type2 = [
 		super: 0,
 		mask: ['reference'],
 		value: 3
+	}
+]
+
+//    () / Function
+// 0: () / Function
+
+let type2 = [
+	{
+		super: undefined,
+		mask: ['predefined'],
+		value: 'Function'
+	}
+]
+
+//    <...>(...) awaits? throws? -> _?
+// 0: <   >      awaits? throws?
+// 1:  ...
+// 2:      (   )
+// 3:       ...
+// 4:                            -> _?
+
+let type3 = [
+	{
+		super: undefined,
+		mask: ['function', 'genericParameters', 'awaits?', 'throws?'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['variadic'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['parameters'],
+		value: undefined
+	},
+	{
+		super: 2,
+		mask: ['variadic'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['return', 'predefined', 'nillable'],
+		value: '_'
+	}
+]
+
+//    ([]..., ...) awaits throws -> _
+// 0: (          ) awaits throws
+// 1:  []...
+// 2:       , ...
+// 3:                            -> _
+
+let type3 = [
+	{
+		super: undefined,
+		mask: ['function', 'awaits', 'throws'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['array', 'variadic'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['variadic'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['return', 'predefined'],
+		value: '_'
+	}
+]
+
+//    (() -> _)?
+// 0: (()     )?
+// 1:     -> _
+
+let type4 = [
+	{
+		super: undefined,
+		mask: ['function', 'nillable'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['return', 'predefined'],
+		value: '_'
+	}
+]
+
+//    [_?]
+// 0: [  ]
+// 1:  _?
+
+let type5 = [
+	{
+		super: undefined,
+		mask: ['array'],
+		value: undefined
+	},
+	{
+		super: 0,
+		mask: ['predefined', 'nillable'],
+		value: '_'
+	}
+]
+
+//    Array<_?>
+// 0: Array<  >
+// 1:       _?
+
+let type6 = [
+	{
+		super: undefined,
+		mask: ['reference', 'genericArguments'],
+		value: 1 // Array's address
+	},
+	{
+		super: 0,
+		mask: ['predefined', 'nillable'],
+		value: '_'
 	}
 ]
