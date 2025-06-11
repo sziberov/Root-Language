4cd58d6587b73e34ddc1baebe36176bfa7d3a6a2
DIES <sziberov@gmail.com>
2025-06-11 04:13:44 +0300

    Update

--- CPP/Includes/Parser.New.cpp
+++ CPP/Includes/Parser.New.cpp
@@ -27,26 +27,41 @@ struct Parser {
 		}
 	};
 
-	enum class State {
-		Raw,
-		Working,
-		Done
-	};
+	enum class State { Raw, Working, Done };
 
-	struct SignedValue {
+	struct Payload {
 		NodeValue value;
-		usize order,
+		isize triviality,
 			  cleanTokens,
 			  dirtyTokens;
+
+		isize size() const {
+			return cleanTokens+dirtyTokens;
+		}
+
+		void addSize(const Payload& other) {
+			cleanTokens += other.cleanTokens;
+			dirtyTokens += other.dirtyTokens;
+		}
+
+		bool empty() const {
+			return cleanTokens == 0 && (dirtyTokens > 0 || value.empty());
+		}
+
+		bool operator>(const Payload& other) const {
+			if(empty() != other.empty())			return !empty();
+			if(triviality != other.triviality)		return triviality < other.triviality;
+			if(cleanTokens != other.cleanTokens)	return cleanTokens > other.cleanTokens;
+			if(dirtyTokens != other.dirtyTokens)	return dirtyTokens < other.dirtyTokens;
+
+			return false;
+		}
 	};
 
 	struct Value {
 		unordered_set<Key, Hasher> callers;
 		State state = State::Raw;
-
-		NodeValue value;
-		usize length,
-			  unsupported;  // Failed tokens count
+		Payload payload;
 	};
 
 	using Cache = unordered_map<Key, Value, Hasher>;
@@ -58,6 +73,8 @@ struct Parser {
 			   queue;
 
 	Parser(deque<Token> tokens) : tokens(filter(tokens, [](auto& t) { return !t.trivia; })) {}
+
+	// ----------------------------------------------------------------
 
 	Token& token() {
 		static Token dummy;
@@ -71,45 +88,50 @@ struct Parser {
 		return position >= tokens.size();
 	}
 
-	NodeSP parse(const NodeRule& nodeRule) {
-		Node node;
+	Payload parse(const NodeRule& nodeRule) {
+		Payload payload = { Node() };
+		Node& node = payload.value;
 		usize start = position;
 
 		for(auto& field : nodeRule.fields) {
-			NodeValue value = parse(field.rule);
+			Payload fieldPayload = parse(field.rule);
 
-			if(value.empty() && !field.optional) {
-				return nullptr;
+			if(fieldPayload.empty() && !field.optional) {
+				return Payload();
 			}
 
-			if(field.title && (!value.empty() || !field.optional)) {
-				node[*field.title] = value;
+			payload.addSize(fieldPayload);
+
+			if(field.title && (!fieldPayload.empty() || !field.optional)) {
+				node[*field.title] = fieldPayload.value;
 			}
 		}
 
-		if(nodeRule.normalize) {
+		if(nodeRule.normalize && node.size() <= 1) {
 			if(node.empty()) {
-				return nullptr;
+				return Payload();
 			}
-			if(node.size() == 1) {
-				return node.begin()->second;
-			}
+
+			payload.value = node.begin()->second;
+
+			return payload;
 		}
 
 		node["range"] = {
 			{"start", (int)start},
-			{"end", (int)(position > 0 ? position-1 : 0)}
+			{"end", (int)(start+payload.size())}
 		};
 
 		if(nodeRule.post) {
 			nodeRule.post(node);
 		}
 
-		return SP(node);
+		return payload;
 	}
 
-	NodeValue parse(const TokenRule& tokenRule) {
+	Payload parse(const TokenRule& tokenRule) {
 		Token& token = this->token();
+		Payload payload = { token.value };
 
 		println("Token at position ", position, ": ", token.type, ", value: ", token.value);
 
@@ -119,13 +141,15 @@ struct Parser {
 
 				if(!regex_match(token.type, r)) {
 					println("[Parser] Token type (", token.type, ") is not matching \"", *tokenRule.type, "\"");
+					payload.dirtyTokens++;
 
-					return nullptr;
+					return payload;
 				}
 			} catch(const regex_error& e) {
 				println("[Parser] Error in token type validator: ", e.what());
+				payload.dirtyTokens++;
 
-				return nullptr;
+				return payload;
 			}
 		}
 		if(tokenRule.value) {
@@ -134,49 +158,60 @@ struct Parser {
 
 				if(!regex_match(token.value, r)) {
 					println("[Parser] Token value (", token.value, ") is not matching \"", *tokenRule.value, "\"");
+					payload.dirtyTokens++;
 
-					return nullptr;
+					return payload;
 				}
 			} catch(const regex_error& e) {
 				println("[Parser] Error in token value validator: ", e.what());
+				payload.dirtyTokens++;
 
-				return nullptr;
+				return payload;
 			}
 		}
 
-		println("Position was: ", (int)position, ", set: ", ++position);
+		payload.cleanTokens++;
 
-		return token.value;
+		return payload;
 	}
 
-	NodeValue parse(const VariantRule& variantRule) {
-		for(auto& rule : variantRule) {
-			NodeValue value = parse(rule);
+	Payload parse(const VariantRule& variantRule) {
+		for(int i = 0; i < variantRule.size(); i++) {
+			Payload payload = parse(variantRule[i]);
 
-			if(!value.empty()) {
-				return value;
+			if(!payload.value.empty()) {
+				payload.triviality = i;
+
+				return payload;
 			}
 		}
 
-		return nullptr;
+		return Payload();
 	}
 
-	bool parseDelimiters(const optional<Rule>& delimiterRule, char policy) {
+	Payload parseDelimiters(const optional<Rule>& delimiterRule, char policy) {
+		Payload payload = { NodeArray() };
+		NodeArray& values = payload.value;
 		const string activePolicy = ".?+*",
 					 maxOnePolicy = ".?",
 					 minOnePolicy = ".+";
 
 		if(!delimiterRule || !activePolicy.contains(policy)) {
-			return true;
+			return payload;
 		}
 
 		bool found = false;
 
 		while(!tokensEnd()) {
-			NodeValue value = parse(*delimiterRule);
+			Payload delimiterPayload = parse(*delimiterRule);
 
-			if(value.empty()) {
+			if(delimiterPayload.empty()) {
+				position -= delimiterPayload.size();
+
 				break;
+			} else {
+				payload.addSize(delimiterPayload);
+				values.push_back(delimiterPayload.value);
 			}
 
 			found = true;
@@ -187,15 +222,17 @@ struct Parser {
 		}
 
 		if(!found && minOnePolicy.contains(policy)) {
-			return false;
+			return Payload();
 		}
 
-		return true;
+		return payload;
 	}
 
-	NodeValue parse(const SequenceRule& sequenceRule) {
-		NodeArray values;
+	Payload parse(const SequenceRule& sequenceRule) {
+		Payload payload = { NodeArray() };
+		NodeArray& values = payload.value;
 
+		/*
 		if(sequenceRule.opener && parse(*sequenceRule.opener).empty()) {
 			return nullptr;
 		}
@@ -246,118 +283,45 @@ struct Parser {
 				return nullptr;
 			}
 		}
+		*/
 
-		return values;
+		return payload;
 	}
 
-	/*
-	NodeValue parse(const SequenceRule& sequenceRule) {
-		if(sequenceRule.opener && parse(*sequenceRule.opener).empty()) {
-			return nullptr;
-		}
-
-		NodeArray values;
-		NodeSP unsupportedNode;
-		int scopeLevel = 0;
-
-		while(!tokensEnd()) {
-			NodeValue value = parse(sequenceRule.rule);
-
-			if(!value.empty()) {
-				values.push_back(value);
-				unsupportedNode = nullptr;
-
-				if(sequenceRule.single) {
-					break;
-				}
-			}
-
-			if(sequenceRule.opener && !parse(*sequenceRule.opener).empty()) {
-				scopeLevel++;
-			} else
-			if(sequenceRule.closer && !parse(*sequenceRule.closer).empty()) {
-				scopeLevel--;
-			}
-
-			if(scopeLevel == 0 || sequenceRule.single || tokensEnd()) {
-				break;
-			}
-
-			if(value.empty()) {
-				if(sequenceRule.unsupported) {
-					if(!unsupportedNode) {
-						unsupportedNode = SP<Node>({
-							{"type", "unsupported"},
-							{"range", {
-								{"start", (int)position},
-								{"end", (int)position}
-							}},
-							{"tokens", NodeArray {}}
-						});
-					}
-
-					position++;
-				} else {
-					break;
-				}
-			}
-		}
-
-		if(sequenceRule.closer && parse(*sequenceRule.closer).empty()) {
-			if(!tokensEnd()) {
-				println("[Parser] Sequence doesn't have the closing token and was decided to be autoclosed at the end of stream");
-			} else {
-				return nullptr;
-			}
-		}
-
-		if(sequenceRule.single) {
-			if(values.empty()) {
-				return nullptr;
-			}
-			if(values.size() == 1) {
-				return values.front();
-			}
-		}
-
-		return values;
-	}
-	*/
-
-	NodeValue parse(const RuleRef& ruleRef) {
+	Payload parse(const RuleRef& ruleRef) {
 		println("[Parser] Rule \"", ruleRef, "\"");
 		if(!Grammar::rules.contains(ruleRef)) {
-			return nullptr;
+			return Payload();
 		}
 
 		Rule& rule = Grammar::rules[ruleRef];
-		NodeValue value = parse(rule);
+		Payload payload = parse(rule);
 
-		if(value.type() == 5) {
-			Node& nodeValue = value;
+		if(payload.value.type() == 5) {
+			Node& node = payload.value;
 
-			if(!nodeValue.contains("type")) {
-				nodeValue["type"] = ruleRef;
+			if(!node.contains("type")) {
+				node["type"] = ruleRef;
 			}
 		}
 
-		println("[Parser] Rule \"", ruleRef, "\": ", to_string(value));
+		println("[Parser] Rule \"", ruleRef, "\": ", to_string(payload.value));
 
-		return value;
+		return payload;
 	}
 
-	NodeValue parseDispatch(const Rule& rule) {
+	Payload parseDispatch(const Rule& rule) {
 		switch(rule.index()) {
 			case 0:  return parse(get<0>(rule));
 			case 1:  return parse(get<1>(rule).get());
 			case 2:  return parse(get<2>(rule).get());
 			case 3:  return parse(get<3>(rule).get());
 			case 4:  return parse(get<4>(rule).get());
-			default: return nullptr;
+			default: return Payload();
 		}
 	}
 
-	NodeValue parse(const Rule& rule) {
+	Payload parse(const Rule& rule) {
 		Key key(rule, position);
 		Value& cacheValue = cache[key];
 
@@ -372,7 +336,7 @@ struct Parser {
 				if(tokensEnd()) {
 					println("[Parser] Reached the end of stream");
 
-					return nullptr;
+					return Payload();
 				}
 
 				cacheValue.state = State::Working;
@@ -388,12 +352,10 @@ struct Parser {
 					queue.pop_front();
 					position = queueKey.start;
 
-					NodeValue queueValueValue = parseDispatch(queueKey.rule);
-					usize queueValueLength = position-queueKey.start;
+					Payload payload = parseDispatch(queueKey.rule);
 
-					if(queueValueLength > queueValue.length) {
-						queueValue.value = queueValueValue;
-						queueValue.length = queueValueLength;
+					if(payload > queueValue.payload) {
+						queueValue.payload = payload;
 
 						for(const Key& callerKey : queueValue.callers) {
 							if(!contains(queue, callerKey)) {
@@ -405,21 +367,23 @@ struct Parser {
 					queueValue.state = State::Done;
 				}
 
-				position = key.start+cacheValue.length;
+				position = key.start+cacheValue.payload.size();
 			break;
 			case State::Working:
-				println("working");
+				if(!cacheValue.payload.empty()) {
+					position = key.start+cacheValue.payload.size();
+				}
 			break;
 			case State::Done:
-				position = key.start+cacheValue.length;
+				position = key.start+cacheValue.payload.size();
 			break;
 		}
 
 		calls.pop_back();
 
-		println("Parsed rule at pos ", key.start, " => length: ", cacheValue.length, ", callers: ", cacheValue.callers.size(), ", value: ", to_string(cacheValue.value));
+		println("Parsed rule at pos ", key.start, " => clean: ", cacheValue.payload.cleanTokens, ", dirty: ", cacheValue.payload.dirtyTokens, ", callers: ", cacheValue.callers.size(), ", value: ", to_string(cacheValue.payload.value));
 
-		return cacheValue.value;
+		return cacheValue.payload;
 	}
 
 	NodeSP parse() {
@@ -430,7 +394,7 @@ struct Parser {
 			{"moduleID", -1}
 		});
 
-		NodeSP tree = parse(RuleRef("module"));
+		NodeSP tree = parse(RuleRef("module")).value;
 
 		Interface::sendToClients({
 			{"type", "notification"},
